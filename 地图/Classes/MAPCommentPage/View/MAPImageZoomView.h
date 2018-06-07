fb584fe328edb5b7218fb10d8591cc3e706f4f92
Cui-ym <1652923531@qq.com>
2018-06-07 20:21:46 +0800

    图片浏览

---
+++ 地图/Classes/MAPCommentPage/View/MAPImageZoomView.h
@@ -0,0 +1,13 @@
+//
+//  MAPImageZoomView.h
+//  地图
+//
+//  Created by 崔一鸣 on 2018/6/7.
+//  Copyright © 2018年 崔一鸣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAPImageZoomView : UIScrollView
+
+@end
---
+++ 地图/Classes/MAPCommentPage/View/MAPImageZoomView.m
@@ -0,0 +1,21 @@
+//
+//  MAPImageZoomView.m
+//  地图
+//
+//  Created by 崔一鸣 on 2018/6/7.
+//  Copyright © 2018年 崔一鸣. All rights reserved.
+//
+
+#import "MAPImageZoomView.h"
+
+@implementation MAPImageZoomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
